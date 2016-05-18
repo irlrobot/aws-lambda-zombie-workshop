0087bb73a7d189bcdd185154a6e0803b85c1a68d
Josh Campbell <joshcb@amazon.com>
2016-05-18 15:36:05 -0500

    adds slack integration

--- ChatServiceLambdaFunctions/ZombiePostMessage.js
+++ ChatServiceLambdaFunctions/ZombiePostMessage.js
@@ -5,7 +5,7 @@ var ddb = new aws.DynamoDB(
     params: {TableName: "messages"}}
 );
 var querystring = require('querystring');
-
+var https = require('https');
 var theContext;
 var message;
 var from;
@@ -17,19 +17,19 @@ var channel = 'default';
 
 exports.handler = function(event, context) {
     theContext = context;
-    
+
     if(event.message == null || event.message == 'null' || event.name == null || event.name == 'null') {
-        return context.fail("Message and Name cannot be null");  
+        return context.fail("Message and Name cannot be null");
     } else {
         message = event.message;
         from = event.name;
     }
-    
+
     if (event.timestamp == null || event.timestamp == 'null') {
         event.timestamp = "" + new Date().getTime();
         timestamp = event.timestamp;
     }
-    
+
     /**
      * For Debubugging input params to the lambda function
     console.log('Message: ' + message);
@@ -45,13 +45,47 @@ exports.handler = function(event, context) {
             "name":{"S":from}
         }
     };
-    
+
     dynamoPut(DDBparams);
+    sendToSlack(message, context);
 };
 
 function dynamoPut(params){
     console.log("Putting item into DynamoDB");
     ddb.putItem(params, dynamoCallback);
+}
+
+function sendToSlack(message, context){
+  var post_data = JSON.stringify({"text": message});
+
+  var post_options = {
+      host: 'hooks.slack.com',
+      port: '443',
+      path: '/services/XXX/XXX/XXX', // your webhook info here
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': post_data.length
+      }
+  };
+
+  var req = https.request(post_options, function(res) {
+      console.log('Status:', res.statusCode);
+      console.log('Headers:', JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', function(chunk) {});
+      req.on('error' , function (err) {
+        console.log( 'Problem with request to Slack: ' + err.message );
+        context.fail();
+      });
+      res.on('end', function() {
+          console.log('Successfully sent message to Slack');
+          context.succeed("Your Slack message was sent. Message sent was: " + message);
+      });
+  });
+
+  req.write(post_data);
+  req.end();
 }
 
 function dynamoCallback(err, response) {
