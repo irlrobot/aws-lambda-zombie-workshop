2536bd3a0536557fc1a8c35867bc34ebd1355420
Josh Campbell <joshcb@amazon.com>
2016-05-18 14:41:16 -0500

    removes whitespace

--- Slack/SlackService.js
+++ Slack/SlackService.js
@@ -20,23 +20,23 @@ var processEvent = function(event, context) {
         console.error("Request token (" + requestToken + ") does not match exptected token for Slack");
         context.fail("Invalid request token");
     }
-    
+
     else {
         var from = params.user_name;
         var command = params.command;
         var slackChannel = params.channel_name;
         var message = params.text;
         var timestamp = "" + new Date().getTime();
-        
+
         // Now that we have the Slack message formatted correctly, we make a request to our Chat Service
         var post_data = JSON.stringify({
-            "message": "Incoming message from Slack user: " + message, 
-            "name": from, 
+            "message": "Incoming message from Slack user: " + message,
+            "name": from,
             "channel": "default",
             "timestamp": timestamp
-               
+
         });
-    
+
         // Object of options to designate where to send our request
         var post_options = {
             host: 'INSERT YOUR API GATEWAY URL HERE EXCLUDING THE HTTPS://',
@@ -48,7 +48,7 @@ var processEvent = function(event, context) {
                 'Content-Length': post_data.length
             }
         };
-    
+
         var req = https.request(post_options, function(res) {
             var body = '';
             console.log('Status:', res.statusCode);
@@ -57,7 +57,7 @@ var processEvent = function(event, context) {
             res.on('data', function(chunk) {
                 body += chunk;
             });
-    
+
             res.on('end', function() {
                 console.log('Successfully processed HTTPS response');
                 // If we know it's JSON, parse it
@@ -66,9 +66,9 @@ var processEvent = function(event, context) {
                 }
                 context.succeed("Your slack message was sent to survivors. Message sent was: " + message);
             });
-            
+
         });
-        
+
         req.on('error', context.fail);
         req.write(post_data);
         req.end();
